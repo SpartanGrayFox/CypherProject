43e829b33f06fc8453692950d067ad690d3ab9d7
SpartanGrayFox <167860669+SpartanGrayFox@users.noreply.github.com>
2024-05-29 20:41:30 -0400

    stuck in this part

---
+++ CodeAcademyCypher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      Console.Write("Give me the secret message ");
+      string message = Console.ReadLine();
+      char[] secretMessage = message.ToCharArray();
+      char[] encryptedMessage = new char[secretMessage.Length];
+      for (int i = 0; secretMessage.Length > i; i++)
+      { char Letters = secretMessage[i];
+        int lettersIndex = Array.IndexOf(alphabet, Letters);
+        char newLetter = alphabet[(lettersIndex + 3) % 26];
+        string encryptedMessageS = String.Join("", encryptedMessage);
+        Console.WriteLine(encryptedMessageS);
+
+
+
+      }
+      
+
+    }
+  }
+}
